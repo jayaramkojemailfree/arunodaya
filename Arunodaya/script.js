8e60500056d4c34cfa332eac35a8b6053cf195d9
jayaramkojemailfree <161569890+jayaramkojemailfree@users.noreply.github.com>
2024-02-28 13:14:13 +0545

    Add files via upload

---
+++ Arunodaya/script.js
@@ -0,0 +1,22 @@
+
+
+const name=document.getElementById('name')
+const age=document.getElementById('age')
+const cnumber=document.getElementById('cnumber')
+const dob=document.getElementById('dob')
+const gender=document.getElementById('gender')
+const email=document.getElementById('email')
+const subject=document.getElementById('subject')
+const fname=document.getElementById('fname')
+const mname=document.getElementById('mname')
+const taddress=document.getElementById('taddress')
+const pschool=document.getElementById('paddress')
+const user_name=document.getElementById('username')
+const password=document.getElementById('password')
+
+
+if(age.value==''){
+    alert('it can')
+
+}
+
